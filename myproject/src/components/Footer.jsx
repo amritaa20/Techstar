49da702c8c1012302ea5709d6c9f8dc734f57302
Amrita <amritaasthana01@gmail.com>
2023-07-05 12:40:04 +0530

    sample commit

---
+++ myproject/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <footer>
+        <div>
+          <h1>TechyStar</h1>
+          <p>@all right reserved</p>
+        </div>
+  
+        <div>
+          <h5>Follow Us</h5>
+          <div>
+            <a href="https://www.linkedin.com/in/amrita-asthana-995153220" target={"blank"}>
+              LinkedIn
+            </a>
+            <a href="https://instagram.com/amrita_asthana" target={"blank"}>
+              Instagram
+            </a>
+            <a href="https://github.com/amritaa20" target={"blank"}>
+              Github
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+  
+
+export default Footer;
